b4d6bbffb8547aba6df0671a991e49cc215beab4
Gabriel Arroyo <ing.gabriel.arroyo@gmail.com>
2020-03-11 23:51:42 -0600

    mod init files

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -13,6 +13,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 desktop=$(echo $DESKTOP_SESSION)
+desktop="bspwm"
 count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
 
 
